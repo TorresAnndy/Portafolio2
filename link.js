2764098afca7366d83b74ce8794c71080125c4cf
TorresAnndy <andygonzales2003@gmail.com>
2024-07-16 16:48:08 -0500

    arreglado

--- link.js
+++ link.js
@@ -1,23 +1,23 @@
-const handleLinkedInPress = () => {
+export const handleLinkedInPress = () => {
     Linking.openURL('https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin');
   };
 
-  const handleGitHubPress = () => {
+export  const handleGitHubPress = () => {
     Linking.openURL('https://github.com/TorresAnndy');
   };
 
-  const handlefacebookPress = () => {
+export  const handlefacebookPress = () => {
     Linking.openURL('https://www.facebook.com/andygonzales.gonzalestorres');
   };
 
-  const handleinstagramPress = () => {
+export  const handleinstagramPress = () => {
     Linking.openURL('https://www.instagram.com/gonzalesangel2003/');
   };
 
-  const handleProyectoPress = () => {
+export  const handleProyectoPress = () => {
     Linking.openURL('https://github.com/TorresAnndy/AccesoaDatosGonzales2.0');
   };
 
-  const handleProyectoCeoyDeskPress = () => {
+export  const handleProyectoCeoyDeskPress = () => {
     Linking.openURL('https://github.com/TorresAnndy/CeoyDesk');
   };
