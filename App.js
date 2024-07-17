a7e4d268d959527626407e80420905036d6855d1
TorresAnndy <andygonzales2003@gmail.com>
2024-07-16 22:05:15 -0500

    refactorizacion

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+// App.js o App.tsx
+import React, { useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Animated, TouchableOpacity, ScrollView, Platform} from 'react-native';
-import * as link from './link.js'; // Importar todas las funciones desde el archivo LinkingUtils
+import { Text, View, Image, Animated, TouchableOpacity, ScrollView } from 'react-native';
+import * as link from './link'; // Importar todas las funciones desde el archivo LinkingUtils
+import { globalStyles } from './styles'; // Importar estilos desde el archivo styles.js o styles.ts
 
 export default function App() {
   const fadeAnim = useRef(new Animated.Value(0)).current;
@@ -19,56 +21,56 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <StatusBar style="dark" />
-      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+      <ScrollView contentContainerStyle={globalStyles.scrollViewContainer}>
         <Animated.View style={{ opacity: fadeAnim }}>
           {/* Header */}
-          <View style={styles.header}>
+          <View style={globalStyles.header}>
             <Image
               source={require('./assets/perfil.png')}
-              style={styles.circularImage}
+              style={globalStyles.circularImage}
             />
-            <Text style={styles.headerText}>Hola, mi nombre es Andy</Text>
-            <Text style={styles.subtitle}>Junior Developer</Text>
+            <Text style={globalStyles.headerText}>Hola, mi nombre es Andy</Text>
+            <Text style={globalStyles.subtitle}>Junior Developer</Text>
           </View>
           {/* Sobre mí */}
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Sobre mí</Text>
-            <Text style={styles.sectionContent}>
+          <View style={globalStyles.section}>
+            <Text style={globalStyles.sectionTitle}>Sobre mí</Text>
+            <Text style={globalStyles.sectionContent}>
               Tengo 21 años, sigo una carrera de Ingenieria en Tecnologias de la Informacion
             </Text>
           </View>
 
           {/* Áreas de Aspiraciones */}
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Áreas de Aspiraciones</Text>
-            <View style={styles.project}>
+          <View style={globalStyles.section}>
+            <Text style={globalStyles.sectionTitle}>Áreas de Aspiraciones</Text>
+            <View style={globalStyles.project}>
               <Image
                 source={require('./assets/Progra.png')}
-                style={styles.projectImage}
+                style={globalStyles.projectImage}
               />
-              <View style={styles.projectInfo}>
-                <Text style={styles.projectDescription}>- Backend</Text>
-                <Text style={styles.projectDescription}>- Ciberseguridad</Text>
+              <View style={globalStyles.projectInfo}>
+                <Text style={globalStyles.projectDescription}>- Backend</Text>
+                <Text style={globalStyles.projectDescription}>- Ciberseguridad</Text>
               </View>
             </View>
           </View>
 
-          <Text style={styles.sectionTitle}>Proyecto</Text>
+          <Text style={globalStyles.sectionTitle}>Proyecto</Text>
 
           {/* Proyecto */}
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>CEOYDESK</Text>
-            <View style={styles.project}>
+          <View style={globalStyles.section}>
+            <Text style={globalStyles.sectionTitle}>CEOYDESK</Text>
+            <View style={globalStyles.project}>
             <TouchableOpacity onPress={link.handleProyectoCeoyDeskPress}>
                 <Image
                   source={require('./assets/s.png')}
-                  style={styles.projectImgbdd}
+                  style={globalStyles.projectImgbdd}
                 />
               </TouchableOpacity>
-              <View style={styles.projectInfo}>
-                <Text style={styles.sectionContent}>
+              <View style={globalStyles.projectInfo}>
+                <Text style={globalStyles.sectionContent}>
                   Sistema de seguridad a nivel empresarial, el cual contiene sistema de seguridad
                   de ingreso, registro vehicular, de la persona, tiempo que ingresa al plantel, sistema de vigilancia
                   24/7, vigilancia aerea con drones
@@ -76,22 +78,22 @@ export default function App() {
               </View>
             </View>
             <TouchableOpacity onPress={link.handleProyectoCeoyDeskPress}>
-              <Text style={styles.botonesgithub}>GitHub</Text>
+              <Text style={globalStyles.botonesgithub}>GitHub</Text>
               </TouchableOpacity>
           </View>
 
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Acceso a Datos</Text>
-            <View style={styles.project}>
+          <View style={globalStyles.section}>
+            <Text style={globalStyles.sectionTitle}>Acceso a Datos</Text>
+            <View style={globalStyles.project}>
             <TouchableOpacity onPress={link.handleProyectoPress}>
                 <Image
                   source={require('./assets/BDD.png')}
-                  style={styles.projectImgbdd}
+                  style={globalStyles.projectImgbdd}
                 />
               </TouchableOpacity>
             
-              <View style={styles.projectInfo}>
-                <Text style={styles.sectionContent}>
+              <View style={globalStyles.projectInfo}>
+                <Text style={globalStyles.sectionContent}>
                   El proyecto permite ingresar, revisar y borrar datos de estudiantes
                   de una universidad almacenando en una base de datos
                 </Text>
@@ -99,79 +101,79 @@ export default function App() {
               </View>
             </View>
             <TouchableOpacity onPress={link.handleProyectoPress}>
-              <Text style={styles.botonesgithub}>GitHub</Text>
+              <Text style={globalStyles.botonesgithub}>GitHub</Text>
               </TouchableOpacity>
           </View>
 
           {/* Aprendido */}
-            <View style={styles.section}>
-              <Text style={styles.sectionTitle}>Aprendizaje</Text>
+            <View style={globalStyles.section}>
+              <Text style={globalStyles.sectionTitle}>Aprendizaje</Text>
               
-              <View style={styles.contactLinks}>
+              <View style={globalStyles.contactLinks}>
                 <Image
                   source={require('./assets/c.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
                 <Image
                   source={require('./assets/cc.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
                 <Image
                   source={require('./assets/java.png')}
-                  style={styles.projectImgcont}
+                  style={globalStyles.projectImgcont}
                 />
                 <Image
                   source={require('./assets/py.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
               </View>
 
-              <View style={styles.contactLinks}>
+              <View style={globalStyles.contactLinks}>
                 <Image
                   source={require('./assets/js.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
                 <Image
                   source={require('./assets/dc.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
                 <Image
                   source={require('./assets/cisco.png')}
-                  style={styles.lenguajescisco}
+                  style={globalStyles.lenguajescisco}
                 />
                 <Image
                   source={require('./assets/vs.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
               </View>
 
-              <View style={styles.contactLinks}>
+              <View style={globalStyles.contactLinks}>
                 <Image
                   source={require('./assets/wk.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
 
                 <Image
                   source={require('./assets/vsc.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
 
                 <Image
                   source={require('./assets/linux.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
 
                 <Image
                   source={require('./assets/ubuntu.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
 
               </View>
 
-              <View style={styles.contactLinks}>
+              <View style={globalStyles.contactLinks}>
               <Image
                   source={require('./assets/eclipse.png')}
-                  style={styles.lenguajes}
+                  style={globalStyles.lenguajes}
                 />
 
               </View>
@@ -180,35 +182,35 @@ export default function App() {
 
 
           {/* Contacto */}
-          <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Contacto</Text>
-            <View style={styles.contactLinks}>
+          <View style={globalStyles.section}>
+            <Text style={globalStyles.sectionTitle}>Contacto</Text>
+            <View style={globalStyles.contactLinks}>
 
               <TouchableOpacity onPress={link.handleLinkedInPress}>
                 <Image
                   source={require('./assets/Linked.png')}
-                  style={styles.projectImgcont}
+                  style={globalStyles.projectImgcont}
                 />
               </TouchableOpacity>
 
               <TouchableOpacity onPress={link.handleGitHubPress}>
                 <Image
                   source={require('./assets/Github.png')}
-                  style={styles.projectImgcont}
+                  style={globalStyles.projectImgcont}
                 />
               </TouchableOpacity>
 
               <TouchableOpacity onPress={link.handlefacebookPress}>
                 <Image
                   source={require('./assets/Facebook.png')}
-                  style={styles.projectImgcont}
+                  style={globalStyles.projectImgcont}
                 />
               </TouchableOpacity>
 
               <TouchableOpacity onPress={link.handleinstagramPress}>
                 <Image
                   source={require('./assets/Instagram.png')}
-                  style={styles.projectImgcont}
+                  style={globalStyles.projectImgcont}
                 />
               </TouchableOpacity>
             </View>
@@ -218,128 +220,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff', 
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? 29 : 0,
-  },
-  scrollViewContainer: {
-    flexGrow: 1,
-    padding: 20,
-  },
-  header: {
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  circularImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    marginBottom: 10,
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 5,
-    color: '#000', 
-  },
-  subtitle: {
-    fontSize: 20,
-    color: '#555',
-    marginBottom: 21,
-  },
-  section: {
-    marginBottom: 30,
-    padding: 10,
-    borderWidth: 2,
-    borderColor: '#007bff',
-    borderRadius: 10,
-  },
-  sectionTitle: {
-    alignItems: 'center',
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 15,
-    color: '#000', 
-  },
-  sectionContent: {
-    alignItems: 'center',
-    textAlign: 'center',
-    fontSize: 16,
-    lineHeight: 22,
-    color: '#000', 
-  },
-  
-  project: {
-    flexDirection: 'row',
-    marginBottom: 20,
-  },
-  projectImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 10,
-    marginRight: 10,
-  },
-  projectImgcont: {
-    width: 50,
-    height: 50,
-    borderRadius: 10,
-    marginRight: 10,
-  },
-  lenguajes: {
-    width: 50,
-    height: 50,
-    borderRadius: 10,
-    marginRight: 10,
-  },
-
-  lenguajescisco: {
-    width: 65,
-    height: 50,
-    borderRadius: 10,
-    marginRight: 10,
-  },
-  botonesgithub: {
-    backgroundColor: '#375ECE',
-    marginBottom: 5,
-    padding: 10,
-    borderWidth: 2,
-    borderColor: '#000',
-    borderRadius: 10,
-    alignSelf: 'center',
-  },
-  projectImgbdd: {
-    width: 85,
-    height: 100,
-    borderRadius: 10,
-    marginRight: 10,
-  },
-  projectInfo: {
-    flex: 1,
-  },
-  projectDescription: {
-    fontSize: 14,
-    marginBottom: 5,
-    color: '#000', 
-  },
-  contactLinks: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 10,
-  },
-  contactLink: {
-    marginHorizontal: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#007bff',
-    borderRadius: 5,
-  },
-  contactLinkText: {
-    color: '#007bff',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,124 @@
+// styles.js
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff', 
+    flex: 1,
+  },
+  scrollViewContainer: {
+    flexGrow: 1,
+    padding: 20,
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  circularImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 10,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#000', 
+  },
+  subtitle: {
+    fontSize: 20,
+    color: '#555',
+    marginBottom: 21,
+  },
+  section: {
+    marginBottom: 30,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: '#007bff',
+    borderRadius: 10,
+  },
+  sectionTitle: {
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    color: '#000', 
+  },
+  sectionContent: {
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 16,
+    lineHeight: 22,
+    color: '#000', 
+  },
+  project: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  projectImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  projectImgcont: {
+    width: 50,
+    height: 50,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  lenguajes: {
+    width: 50,
+    height: 50,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  lenguajescisco: {
+    width: 65,
+    height: 50,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  botonesgithub: {
+    backgroundColor: '#375ECE',
+    marginBottom: 5,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: '#000',
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  projectImgbdd: {
+    width: 85,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  projectInfo: {
+    flex: 1,
+  },
+  projectDescription: {
+    fontSize: 14,
+    marginBottom: 5,
+    color: '#000', 
+  },
+  contactLinks: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  contactLink: {
+    marginHorizontal: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#007bff',
+    borderRadius: 5,
+  },
+  contactLinkText: {
+    color: '#007bff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
